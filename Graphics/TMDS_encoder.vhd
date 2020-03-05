88fad9c4586ac8b5f11531cb61a081061378dc44
DaKnig <37626476+DaKnig@users.noreply.github.com>
2020-03-05 12:20:30 +0200

    added a Makefile for working with ghdl fixed a bug

--- Graphics/TMDS_encoder.vhd
+++ Graphics/TMDS_encoder.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 
-entity TMDS_pixel_encoder is
+entity TMDS_encoder is
   
   port (
     data_in     : in  unsigned(7 downto 0);  -- serial data in, clocked at the pixel clock
@@ -13,20 +13,19 @@ entity TMDS_pixel_encoder is
     bit_clock   : in  std_logic;  -- 10x pixel clock
     pixel_clock : out std_logic);
 
-end entity TMDS_pixel_encoder;
+end entity TMDS_encoder;
 
-architecture behave of TMDS_pixel_encoder is
+architecture behave of TMDS_encoder is
 
   signal data_reduced_transitions: unsigned(7 downto 0);
-
+  signal bit_counter : integer range 0 to 9;
 begin  -- architecture behave
 
   -- purpose: generate data_reduced_transition
   -- type   : sequential
   -- inputs : bit_clock, rst, data_in
   -- outputs: data_reduced_transitions
-  process (bit_clock, rst)
-    signal bit_counter : integer range 0 to 9;
+  process (bit_clock, rst) is
   begin  -- process
     if rst = '0' then                   -- asynchronous reset (active low)
       bit_counter <= 0;
